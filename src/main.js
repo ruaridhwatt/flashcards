0b6abef2470fceae1b0b5a64a35878b80ffa9874
Ruaridh Watt <ruaridh.watt@gmail.com>
2023-11-22 14:46:02 +0100

    firebase init emulators

--- src/main.js
+++ src/main.js
@@ -5,11 +5,8 @@ import { createApp, ref } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { initializeApp } from 'firebase/app';
-import { firebaseConfig } from '@/firebaseConfig';
 
 let vueApp = null;
-const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
 export const user = ref(null);
 
